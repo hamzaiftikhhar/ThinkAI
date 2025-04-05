f7c6c33aca65eb7d359f57723cc2603954ffe5a5
Hamzaiftikhhar <hamzaiftikhhar@gmail.com>
2025-04-05 19:47:08 +0500

    Provided paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type React from "react"
 import "./globals.css"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+// import { ThemeProvider } from "../components/theme-provider"
+import { ThemeProvider } from "next-themes"
 import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
